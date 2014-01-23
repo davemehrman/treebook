002beb38c8ffc5cb546823bf13ac733ab766fcb6
Dave Mehrman <dmehrman@gmail.com>
2014-01-22 22:56:48 -0600

    Added simple form and fixed devise strong parameters issue

---
+++ lib/user_sanitizer.rb
@@ -0,0 +1,10 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+    private
+
+    def sign_up
+        default_params.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :current_password)
+    end
+    def acount_update
+        default_params.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :current_password)
+    end
+end
